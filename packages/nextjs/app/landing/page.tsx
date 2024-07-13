576250e96b6b72e2730d4aed9377c19f6825a7ef
Diego Aguilar <d.aguilaro99@gmail.com>
2024-07-13 20:46:02 +0200

    updated hero

--- packages/nextjs/app/landing/page.tsx
+++ packages/nextjs/app/landing/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import {Header} from "../../components/Header"; 
 import Hero from "../../components/Hero"; 
 const Landing = () => {
   return (
     <div className="relative h-screen w-screen">
-      <Header />
       <Hero />
     </div>
   );
--- packages/nextjs/components/Hero.tsx
+++ packages/nextjs/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="absolute inset-0 z-0">
       <Spline
-        scene="https://prod.spline.design/4qC5bmGdtb9aXyJg/scene.splinecode"
+        scene="https://prod.spline.design/4qC5bmGdtb9aXyJg/scene.splinecode" 
         style={{ width: '100%', height: '100%' }}
       />
     </div>
