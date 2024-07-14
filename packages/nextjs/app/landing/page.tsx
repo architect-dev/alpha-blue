9f472ac87eee1b51433f41d015ffa648db750019
Diego Aguilar <d.aguilaro99@gmail.com>
2024-07-14 05:26:43 +0200

    minor: update

--- packages/nextjs/app/landing/page.tsx
+++ packages/nextjs/app/landing/page.tsx
@@ -25,7 +25,7 @@ const Landing = () => {
       {/* Middle Section */}
       <div className="middle-section flex flex-col items-center justify-center h-screen bg-blue-800 text-white text-center p-8">
         <div className="max-w-5xl p-20 bg-transparent text-lg md:text-xl font-sans font-semibold leading-relaxed text-white">
-          Alpha Blue is a decentralized orderbook exchange that allows two parties to place and fill offers for tokens and NFTs. The protocol leverages Chainlink CCIP for cross-chain interoperability which allows for reliable and secure message transfers. This makes Alpha Blue a superior option for users looking trade and even bridge assets across blockchains. Alpha Blue addresses the gap in the market by providing a user-friendly and secure solution for peer to peer market activity.
+          Alpha Blue is a decentralized orderbook exchange that allows two parties to place and fill offers for tokens and NFTs. The protocol leverages Chainlink CCIP for cross-chain interoperability which allows for reliable and secure message transfers between chains. This makes Alpha Blue a superior option for users looking to trade or even bridge assets across blockchains. Alpha Blue addresses the gap in the market by providing a user-friendly and secure solution for peer to peer market activity.
         </div>
       </div>
 
