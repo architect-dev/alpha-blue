67a521d9892c61e03a2db825b8d22381ea92b886
Diego Aguilar <d.aguilaro99@gmail.com>
2024-07-14 05:24:57 +0200

    text update

--- packages/nextjs/app/landing/page.tsx
+++ packages/nextjs/app/landing/page.tsx
@@ -18,14 +18,14 @@ const Landing = () => {
           style={{ objectFit: 'contain', margin: 40 }}
         />
         <p className="text-lg md:text-xl mb-6">
-          Trade across blockchains directly with the community
+          Trade tokens and NFTs directly with your community
         </p>
       </div>
 
       {/* Middle Section */}
       <div className="middle-section flex flex-col items-center justify-center h-screen bg-blue-800 text-white text-center p-8">
-        <div className="max-w-3xl p-6 bg-transparent text-lg md:text-xl font-sans font-semibold leading-relaxed text-white">
-          Alpha Blue is a decentralized, trustless platform that doesn’t require liquidity on both sides for swaps. With Chainlink CCIP, our cross-chain transactions are fast and reliable, making Alpha Blue a superior option for users looking to bridge assets across multiple blockchains. Alpha Blue addresses the gap in the market by providing a user-friendly and secure solution for truly decentralized asset bridging.
+        <div className="max-w-5xl p-20 bg-transparent text-lg md:text-xl font-sans font-semibold leading-relaxed text-white">
+          Alpha Blue is a decentralized orderbook exchange that allows two parties to place and fill offers for tokens and NFTs. The protocol leverages Chainlink CCIP for cross-chain interoperability which allows for reliable and secure message transfers. This makes Alpha Blue a superior option for users looking trade and even bridge assets across blockchains. Alpha Blue addresses the gap in the market by providing a user-friendly and secure solution for peer to peer market activity.
         </div>
       </div>
 
